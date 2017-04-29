809fc465dfaf97a22f4d945e349a177f9018b0c5
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-04-30 02:19:58 +1000

    add AWS info

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,10 +3,10 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAJGNDYO4PQUKPRNYQ'],
-      :aws_secret_access_key => ENV['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'],
-      :region                => ENV['ap-southeast-2']
+      :aws_access_key_id     => 'AKIAJGNDYO4PQUKPRNYQ',
+      :aws_secret_access_key => '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',
+      :region                => 'ap-southeast-2'
     }
-    config.fog_directory     =  ENV['h3rmes']
+    config.fog_directory     =  'h3rmes'
   end
 end
