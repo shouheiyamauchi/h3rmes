b32bba2b59178278542aa8cfd451946b6c4fbab9
Hyun Kim <hkim@live.com.au>
2017-04-29 22:58:25 +1000

    added order functionality

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,4 +1,11 @@
 class MenuItem < ApplicationRecord
   belongs_to :menu_group
   mount_uploader :image, ImageUploader
+
+  def self.search(search)
+    if search == nil
+    else
+    where("name LIKE ?", "%#{search}%")
+    end
+  end
 end
