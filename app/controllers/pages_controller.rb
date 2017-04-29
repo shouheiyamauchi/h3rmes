a986d46b09c557931686e9e92cc8991e273b530c
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-04-30 03:51:52 +1000

    json test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def json
+  respond_to do |format|
+    msg = {"messages": [{"text": "Welcome to our store!"},{"text": "How can I help you?"}]}
+    format.json  { render :json => msg } # don't do msg.to_json
+  end
+end
 end
