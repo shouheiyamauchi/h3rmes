60c29a2475e9202bc520b4cf44c8fefa5fda0b24
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-04-30 03:55:30 +1000

    json update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,34 @@ class PagesController < ApplicationController
 
   def json
   respond_to do |format|
-    msg = {"messages": [{"text": "Welcome to our store!"},{"text": "How can I help you?"}]}
+    msg = {
+    "messages": [
+        {
+          "attachment": {
+            "type": "template",
+            "payload": {
+              "template_type": "button",
+              "text": "Total Price: <Insert Total>
+
+              < Display total items >
+              Would you like to pay now?",
+              "buttons": [
+                {
+                  "type": "show_block",
+                  "block_name": "Pay",
+                  "title": "Yes"
+                },
+                {
+                  "type": "show_block",
+                  "block_name": "Order - FB Cafe",
+                  "title": "No"
+                }
+              ]
+            }
+          }
+        }
+      ]
+    }
     format.json  { render :json => msg } # don't do msg.to_json
   end
 end
