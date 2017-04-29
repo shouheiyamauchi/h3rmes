cc77aca93e29f56de02b2928f56c829473d771b0
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-04-29 17:50:50 +1000

    added user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+
   resources :orders
   resources :menu_items
   resources :menu_groups
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
