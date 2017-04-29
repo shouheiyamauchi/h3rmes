e7d4e970ae499f25c2c15d846e2129ee8531d053
Hyun Kim <hkim@live.com.au>
2017-04-29 17:50:55 +1000

    added menu group and menu item

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :orders
-  resources :menu_items
-  resources :menu_groups
+  resources :menu_groups do
+    member do
+      resources :menu_items
+    end
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
