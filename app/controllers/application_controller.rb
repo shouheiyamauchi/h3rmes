3f445a79d886b58931c0cdf922cd6039c96d9057
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-05-06 23:54:07 +1000

    authenticity edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token
   before_action :set_menu_nav
 
   def set_menu_nav
