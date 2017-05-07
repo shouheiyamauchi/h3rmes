bd58eda00bc74c1ac38c726e431dfab8d31a44ee
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-05-08 00:14:07 +1000

    JSON

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,160 +1,160 @@
 class PagesController < ApplicationController
-  # def home
-  # end
-  #
-  # def json
-  #   @variable = "hello there"
-  #   respond_to do |format|
-  #     msg = {
-  #     "messages": [
-  #         {
-  #           "attachment": {
-  #             "type": "template",
-  #             "payload": {
-  #               "template_type": "button",
-  #               "text": "Total Price: #{@variable}<Insert Total>
-  #
-  #               < Display total items >
-  #               Would you like to pay now?",
-  #               "buttons": [
-  #                 {
-  #                   "type": "show_block",
-  #                   "block_name": "Pay",
-  #                   "title": "Yes"
-  #                 },
-  #                 {
-  #                   "type": "show_block",
-  #                   "block_name": "Order - FB Cafe",
-  #                   "title": "No"
-  #                 }
-  #               ]
-  #             }
-  #           }
-  #         }
-  #       ]
-  #     }
-  #     format.json  { render :json => msg } # don't do msg.to_json
-  #   end
-  # end
-  #
-  # def test
-  #   @user_id = params[:user_id]
-  #   @fb_id = params[:fb_id]
-  #   respond_to do |format|
-  #     msg = {
-  #     "messages": [
-  #       {"text": "Welcome to our store! User: #{@user_id}, FB ID: #{@fb_id}"},
-  #       {"text": "How can I help you?"}
-  #       ]
-  #     }
-  #     format.json  { render :json => msg } # don't do msg.to_json
-  #   end
-  # end
-  #
-  # def choose_business
-  #   @business = params[:business]
-  #   @table_no = params[:table_no]
-  #   respond_to do |format|
-  #     msg = {
-  #     "messages": [
-  #       {"text": "Welcome to our #{@business}! You're at table no: #{@table_no}"},
-  #       {
-  #         "attachment": {
-  #           "payload":{
-  #             "template_type": "button",
-  #             "text": "test JSON with postback",
-  #             "buttons": [
-  #               {
-  #                 "url": "https://pacific-wave-33803.herokuapp.com/pages/test.json?fb_id={{messenger user id}}",
-  #                 "type":"json_plugin_url",
-  #                 "title":"go"
-  #               }
-  #             ]
-  #           },
-  #           "type": "template"
-  #         }
-  #       }
-  #       ]
-  #     }
-  #     format.json  { render :json => msg } # don't do msg.to_json
-  #   end
-  # end
-  #
-  # def list_categories
-  #   respond_to do |format|
-  #     msg = {
-  #       "messages": [
-  #         {
-  #           "attachment": {
-  #             "type": "template",
-  #             "payload": {
-  #               "template_type": "button",
-  #               "text": "Hello!",
-  #               "buttons": [
-  #                 {
-  #                   "type": "web_url",
-  #                   "url": "https://petersapparel.parseapp.com/buy_item?item_id=100",
-  #                   "title": "Buy Item"
-  #                 }
-  #               ]
-  #             }
-  #           }
-  #         }
-  #       ]
-  #     }
-  #
-  #     MenuGroup.all.each do |category|
-  #       msg[:messages][0][:attachment][:payload][:buttons] << {
-  #               "url": "https://pacific-wave-33803.herokuapp.com/pages/list_foods.json?category_id=#{category.id}",
-  #               "type":"json_plugin_url",
-  #               "title":"#{category.name}"
-  #             }
-  #     end
-  #
-  #     format.json  { render :json => msg } # don't do msg.to_json
-  #   end
-  # end
-  #
-  # def list_foods
-  #   @category_id = params[:category_id]
-  #   respond_to do |format|
-  #     msg = {
-  #      "messages": [
-  #         {
-  #           "attachment":{
-  #             "type":"template",
-  #             "payload":{
-  #               "template_type":"generic",
-  #               "elements":[]
-  #             }
-  #           }
-  #         }
-  #       ]
-  #     }
-  #
-  #     MenuItem.where(:menu_group_id=>@category_id).each do |item|
-  #       msg[:messages][0][:attachment][:payload][:elements] << {
-  #         "title":"#{item.name}...$#{item.price}",
-  #         "image_url":"#{item.image.url}",
-  #         "subtitle":"#{item.description}",
-  #         "buttons":[
-  #           {
-  #             "type":"web_url",
-  #             "url":"https://petersapparel.parseapp.com/view_item?item_id=100",
-  #             "title":"View Item"
-  #           },
-  #           {
-  #             "type":"web_url",
-  #             "url":"https://petersapparel.parseapp.com/buy_item?item_id=100",
-  #             "title":"Buy Item"
-  #           }
-  #         ]
-  #       }
-  #     end
-  #
-  #     format.json  { render :json => msg } # don't do msg.to_json
-  #   end
-  # end
+  def home
+  end
+
+  def json
+    @variable = "hello there"
+    respond_to do |format|
+      msg = {
+      "messages": [
+          {
+            "attachment": {
+              "type": "template",
+              "payload": {
+                "template_type": "button",
+                "text": "Total Price: #{@variable}<Insert Total>
+
+                < Display total items >
+                Would you like to pay now?",
+                "buttons": [
+                  {
+                    "type": "show_block",
+                    "block_name": "Pay",
+                    "title": "Yes"
+                  },
+                  {
+                    "type": "show_block",
+                    "block_name": "Order - FB Cafe",
+                    "title": "No"
+                  }
+                ]
+              }
+            }
+          }
+        ]
+      }
+      format.json  { render :json => msg } # don't do msg.to_json
+    end
+  end
+
+  def test
+    @user_id = params[:user_id]
+    @fb_id = params[:fb_id]
+    respond_to do |format|
+      msg = {
+      "messages": [
+        {"text": "Welcome to our store! User: #{@user_id}, FB ID: #{@fb_id}"},
+        {"text": "How can I help you?"}
+        ]
+      }
+      format.json  { render :json => msg } # don't do msg.to_json
+    end
+  end
+
+  def choose_business
+    @business = params[:business]
+    @table_no = params[:table_no]
+    respond_to do |format|
+      msg = {
+      "messages": [
+        {"text": "Welcome to our #{@business}! You're at table no: #{@table_no}"},
+        {
+          "attachment": {
+            "payload":{
+              "template_type": "button",
+              "text": "test JSON with postback",
+              "buttons": [
+                {
+                  "url": "https://pacific-wave-33803.herokuapp.com/pages/test.json?fb_id={{messenger user id}}",
+                  "type":"json_plugin_url",
+                  "title":"go"
+                }
+              ]
+            },
+            "type": "template"
+          }
+        }
+        ]
+      }
+      format.json  { render :json => msg } # don't do msg.to_json
+    end
+  end
+
+  def list_categories
+    respond_to do |format|
+      msg = {
+        "messages": [
+          {
+            "attachment": {
+              "type": "template",
+              "payload": {
+                "template_type": "button",
+                "text": "Hello!",
+                "buttons": [
+                  {
+                    "type": "web_url",
+                    "url": "https://petersapparel.parseapp.com/buy_item?item_id=100",
+                    "title": "Buy Item"
+                  }
+                ]
+              }
+            }
+          }
+        ]
+      }
+
+      MenuGroup.all.each do |category|
+        msg[:messages][0][:attachment][:payload][:buttons] << {
+                "url": "https://pacific-wave-33803.herokuapp.com/pages/list_foods.json?category_id=#{category.id}",
+                "type":"json_plugin_url",
+                "title":"#{category.name}"
+              }
+      end
+
+      format.json  { render :json => msg } # don't do msg.to_json
+    end
+  end
+
+  def list_foods
+    @category_id = params[:category_id]
+    respond_to do |format|
+      msg = {
+       "messages": [
+          {
+            "attachment":{
+              "type":"template",
+              "payload":{
+                "template_type":"generic",
+                "elements":[]
+              }
+            }
+          }
+        ]
+      }
+
+      MenuItem.where(:menu_group_id=>@category_id).each do |item|
+        msg[:messages][0][:attachment][:payload][:elements] << {
+          "title":"#{item.name}...$#{item.price}",
+          "image_url":"#{item.image.url}",
+          "subtitle":"#{item.description}",
+          "buttons":[
+            {
+              "type":"web_url",
+              "url":"https://petersapparel.parseapp.com/view_item?item_id=100",
+              "title":"View Item"
+            },
+            {
+              "type":"web_url",
+              "url":"https://petersapparel.parseapp.com/buy_item?item_id=100",
+              "title":"Buy Item"
+            }
+          ]
+        }
+      end
+
+      format.json  { render :json => msg } # don't do msg.to_json
+    end
+  end
   #
   # def create_order
   #   @order = Order.new :table_number => params[:table_number], :fb_id => params[:fb_id], :business_name => params[:business_name]
