47a8ef54e737c8137d0ff61895e74c817e986a13
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-07-23 09:02:57 +1000

    test number of items that can go into list

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -50,14 +50,13 @@ class PagesController < ApplicationController
             }
           ]
         }
-        10.times do
-          @business.all.each do |business|
-            msg[:messages][0][:attachment][:payload][:buttons] << {
-                    "url": "#{ENV["APP_URL"]}/pages/create_order.json?business_id=#{business.id}&fb_user=#{@fb_user}&business_id=#{business.id}&table_number=#{@table_number}",
-                    "type":"json_plugin_url",
-                    "title":"#{business.name}"
-                  }
-          end
+        
+        @business.all.each do |business|
+          msg[:messages][0][:attachment][:payload][:buttons] << {
+                  "url": "#{ENV["APP_URL"]}/pages/create_order.json?business_id=#{business.id}&fb_user=#{@fb_user}&business_id=#{business.id}&table_number=#{@table_number}",
+                  "type":"json_plugin_url",
+                  "title":"#{business.name}"
+                }
         end
 
         msg[:messages][0][:attachment][:payload][:buttons] << {
