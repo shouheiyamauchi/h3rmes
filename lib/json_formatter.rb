cca781c90cf7ed97b4515b9399c9b9481b3338ad
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-07-23 18:47:09 +1000

    extract out generating list json to separate method

--- lib/json_formatter.rb
+++ lib/json_formatter.rb
@@ -1,11 +1,53 @@
 class JsonFormatter
 
-  def self.message
-    "Check if this works"
+  def self.display_business_list
+    # business_list = {
+    #  "messages": [
+    #     {
+    #       "attachment":{
+    #         "type":"template",
+    #         "payload":{
+    #           "template_type":"generic",
+    #           "elements":[]
+    #         }
+    #       }
+    #     }
+    #   ]
+    # }
+    #
+    # User.all.each do |business|
+    #   business_list[:messages][0][:attachment][:payload][:elements] << {
+    #     "title":"#{business.name}",
+    #     "image_url":"",
+    #     "subtitle":"",
+    #     "buttons":[
+    #       {
+    #         "type": "json_plugin_url",
+    #         "url": "#{ENV["APP_URL"]}/pages/create_order.json?business_id=#{business.id}&fb_user=#{@fb_user}&business_id=#{business.id}&table_number=#{@table_number}",
+    #         "title": "Check in"
+    #       }
+    #     ]
+    #   }
+    # end
+    #
+    # business_list
+
+    business_list = []
+
+    User.all.each do |business|
+      business_list << {
+        "title": business.name,
+        "button_title": "Check in"
+      }
+    end
+
+    generate_sliding_list_json(business_list)
   end
 
-  def self.display_business_list
-    business_list = {
+  private
+
+  def self.generate_sliding_list_json(data)
+    sliding_list = {
      "messages": [
         {
           "attachment":{
@@ -19,22 +61,22 @@ class JsonFormatter
       ]
     }
 
-    User.all.each do |business|
-      business_list[:messages][0][:attachment][:payload][:elements] << {
-        "title":"#{business.name}",
-        "image_url":"",
-        "subtitle":"",
+    data.each do |item|
+      sliding_list[:messages][0][:attachment][:payload][:elements] << {
+        "title": item[:title],
+        "image_url": item[:image_url],
+        "subtitle": item[:subtitle],
         "buttons":[
           {
             "type": "json_plugin_url",
-            "url": "#{ENV["APP_URL"]}/pages/create_order.json?business_id=#{business.id}&fb_user=#{@fb_user}&business_id=#{business.id}&table_number=#{@table_number}",
-            "title": "Check in"
+            "url": "#{ENV["APP_URL"]}/pages/create_order.json",
+            "title": item[:button_title]
           }
         ]
       }
     end
 
-    business_list
+    sliding_list
   end
 
 end
