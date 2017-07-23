bacaa7b3976d009cde878adbdfea230699f60a98
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-07-23 17:46:24 +1000

    add require class line in application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
-require_relative '../lib/api.rb'
+# require_relative '../lib/api.rb'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -12,5 +12,6 @@ module H3rmes
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths += Dir[Rails.root.join('lib')]
   end
 end
